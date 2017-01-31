4af6c95049cd9fb4d022534b3aed3fb16b85f403
Wojciech Ciesielski <wojciech.ciesielski@antigro.com>
2017-01-31 18:50:03 +0100

    initial

---
+++ src/main/java/pl/ciesielski/sandbox/RibbonSandboxApplication.java
@@ -0,0 +1,12 @@
+package pl.ciesielski.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonSandboxApplication.class, args);
+	}
+}
